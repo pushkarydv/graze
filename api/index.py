2a4511daac956b3de89db9b0358144d0a13e5ad1
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2024-07-24 23:24:33 +0530

    chore: Update Flask templates for homepage and product page styling

--- api/index.py
+++ api/index.py
@@ -1,8 +1,8 @@
 from flask import Flask, jsonify, request, render_template
-
+from flask_minify import Minify
 
 app = Flask(__name__)
-
+Minify(app=app, html=True, js=True, cssless=True)
 
 # Route served as a static page
 @app.route('/')
